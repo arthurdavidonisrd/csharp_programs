519bbf8c776fc2e2d2b3b7f03d5b7635f2b82d34
Arthur Otavio Rodrigues Davidonis <davidonis007gold@gmail.com>
2024-01-23 22:18:24 -0300

    .

---
+++ equacao_1_grau/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Fórmula da equação: ax + b = 0");
+        Console.WriteLine("=================================");
+
+        Console.WriteLine("Defina o valor de A:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("=================================");
+
+        Console.WriteLine("Defina o valor de B");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("=================================");
+
+
+        if(a == 0)
+        {
+            Console.WriteLine("A equação nao é do primeiro grau pois A nao pode ser = 0 ");
+        }
+        else
+        {
+            double x = -b / a;
+            Console.WriteLine($"{a}x + {b} = 0 -> {Math.Abs(x)}");
+
+        }
+       
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ guessing_number/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        bool loop_play = true;
+        int max = 100;
+        int min = 1;
+        int cpu =  random.Next(min,max);
+        int user_guess = 0;
+
+        while (loop_play)
+        {
+
+            while (user_guess != cpu)
+            {
+                Console.WriteLine("Digite umm número:");
+                user_guess = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if(user_guess > cpu)
+                {
+                    Console.WriteLine($"Seu chute {user_guess} foi ALTO");
+
+                }
+                else if (user_guess < cpu)
+                {
+                    Console.WriteLine($"Seu chute {user_guess} foi BAIXO");
+
+                }
+                else
+                {
+                     if(user_guess == cpu)
+                    {
+                        Console.WriteLine("Seu chute foi certeiro");
+                        break;
+                    }
+                }
+
+
+
+            }
+
+        }
+    }
+}
